e5d7b2789debea9737a887c0bfc6a16a5102d7c1
adityabandukeRapidops <aditya.banduke@rapidops.co>
2024-03-19 23:42:40 +0530

    implementSearch in frontend

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -1,9 +1,226 @@
+// import React, { useState, useEffect } from 'react';
+// import Sidebar from '../components/sidebar';
+// import axios from 'axios';
+// import './dashboard.css';
+// import {useNavigate} from 'react-router-dom'
+// import dashboard from '../assets/Group.png'
+
+// const Dashboard = () => {
+//     const [tableData, setTableData] = useState([]);
+//     const [statusFilter, setStatusFilter] = useState('All');
+//     const [selectedUser, setSelectedUser] = useState('All');
+//     const [userOptions, setUserOptions] = useState([]);
+//     const [key, setKey] = useState(0);
+//     const [searchQuery, setSearchQuery] = useState('');
+
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+
+//         const fetchTableData = async () => {
+//             try {
+//                 const response = await axios.get(`http://localhost:8000/rapidops/api/htmlFile/getHtmlbyfilter?status=${statusFilter}&user=${selectedUser}`);
+//                 console.log(selectedUser, statusFilter)
+//                 setTableData(response.data);
+//                 console.log(response.data)
+//             } catch (error) {
+//                 console.error('Error fetching table data:', error);
+//             }
+//         };
+//         console.log(key)
+
+//         fetchTableData();
+//     }, [statusFilter, selectedUser,key]);
+
+
+    
+
+    
+
+//     useEffect(() => {
+//         const html = localStorage.getItem('htmlId');
+//         if(html){
+//             localStorage.removeItem('htmlId')
+
+//         }
+//         // const uid = localStorage.getItem('uid');
+//         // if(uid){
+            
+//         // }
+//         const fetchUsers = async () => {
+//             try {
+//                 const response = await axios.get('http://localhost:8000/rapidops/api/users/getAllUsers');
+//                 setUserOptions(response.data);
+                
+//             } catch (error) {
+//                 console.error('Error fetching users:', error);
+//             }
+//         };
+
+//         fetchUsers();
+//     }, []);
+
+//     const handleStatusChange = (event) => {
+//         setStatusFilter(event.target.value);
+//     };
+
+//     const filteredTableData = tableData.filter((row) =>
+//     Object.values(row).some(
+//         (value) =>
+//             typeof value === 'string' &&
+//             value.toLowerCase().includes(searchQuery.toLowerCase())
+//     )
+// );
+
+
+//     const handleUserChange = (event) => {
+//         setSelectedUser(event.target.value);
+//     };
+
+//     const handleSearch = (event) => {
+//         setSearchQuery(event.target.value);
+//     };
+
+
+//     const getStatusColorClass = (status) => {
+//         switch (status) {
+//             case 'draft':
+//                 return 'status-draft';
+//             case 'published':
+//                 return 'status-published';
+//             case 'scheduled':
+//                 return 'status-scheduled';
+//             default:
+//                 return ''; // No specific class for other status values
+//         }
+//     };
+    
+//     const handleDeleteRow = async (rowId) => {
+//         console.log(rowId)
+//         try {
+//             const deleteHtml = await axios.delete(`http://localhost:8000/rapidops/api/htmlFile/deleteHtml/${rowId}`);
+//             console.log(deleteHtml)
+
+//             const updatedTableData = tableData.filter(row => row.id !== rowId);
+//             setTableData(updatedTableData);
+//             setKey(prevKey => prevKey + 1);
+//         } catch (error) {
+//             console.error('Error deleting row:', error);
+//         }
+//     };
+
+
+
+//     const handleEditRow = (rowId) => {
+//         // Redirect to the edit page for the selected row or call edit API
+//         localStorage.setItem('htmlId' , rowId)
+        
+//         navigate('/homePage')
+//         console.log('Editing row:', rowId);
+
+//     };
+
+//     return (
+//         <div className='dashboardContainer'>
+//             <Sidebar />
+//             <div className='content'>
+//                 <div className='navBar' style={{border:'1px solid #E5E7EB'}}>
+//                     <div className='leftSideItems'>
+//                         <div>logo</div>
+//                         <div className='left'>
+//                         <span class="h4 font-weight-bold">Pages</span>
+
+//                         <span>create and publish pages</span>
+//                         </div>
+//                     </div>
+//                     <div className='rightSideItems'>
+//                         <button type="button" className="publishButton" onClick={()=>{
+//                                     navigate('/homePage')
+
+//                         }}>+ Add Page</button>
+//                     </div>
+//                 </div>
+
+//                 <div className='mainContent'>
+//                     <div className='searchRow'>
+//                     <input
+//                             type="text"
+//                             placeholder="Search..."
+//                             className="search"
+//                             value={searchQuery}
+//                             onChange={handleSearch}
+//                         />
+//                         <span>Entries: {tableData.length}</span>
+//                         <select value={statusFilter} onChange={handleStatusChange}>
+//                             <option value="All">All</option>
+//                             <option value="draft">Draft</option>
+//                             <option value="published">Published</option>
+//                             <option value="scheduled">Scheduled</option>
+//                         </select>
+//                         <select value={selectedUser} onChange={handleUserChange}>
+//                             <option value="All">All Users</option>
+//                             {userOptions.map((user) => (
+//                                 <option key={user.id} value={user.id}>{user.name}</option>
+//                             ))}
+//                         </select>
+//                     </div>
+
+//                     <div className='tableWrapper'>
+//                         <table className='dashboardTable'>
+//                             <thead className='bg-white' >
+//                                 <tr >
+//                                     <th scope="col">#</th>
+//                                     <th scope="col">Title</th>
+//                                     <th scope="col">URL</th>
+//                                     <th scope="col">Created By</th>
+//                                     <th scope="col">Created At</th>
+//                                     <th scope="col">Modified By</th>
+//                                     <th scope="col">Modified At</th>
+//                                     <th scope="col">Status</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 {tableData.map((row, index) => (
+//                                     <tr key={index}>
+//                                         <th scope="row">{index + 1}</th>
+//                                         <td className='tdtitle'>
+//                                             <div>
+//                                                 {row.title}
+//                                             </div>
+//                                             <div class="dropdown" style={{display:'flex' , alignItems:'center' , justifyContent:'center'}}>
+//                                                 <button class="btn " type="button" id={`dropdownMenuButton-${index}`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+//                                                     ...
+//                                                 </button>
+//                                                 <div class="dropdown-menu"  aria-labelledby={`dropdownMenuButton-${index}`}>
+//                                                     <a class="dropdown-item" href="#" onClick={() => handleDeleteRow(row._id)} style={{color : 'red'}}>delete</a>
+//                                                     <a class="dropdown-item" href="#" onClick={() => handleEditRow(row._id)}>edit</a>
+//                                                 </div>
+//                                             </div>
+//                                         </td>
+//                                         <td>{row.endPoint}</td>
+//                                         <td>{row.createdBy}</td>
+//                                         <td>{row.createdAt}</td>
+//                                         <td>{row.modifiedBy}</td>
+//                                         <td>{row.modifiedAt}</td>
+//                                         <td className={getStatusColorClass(row.status)}>{row.status}</td>
+//                                     </tr>
+//                                 ))}
+//                             </tbody>
+//                         </table>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Dashboard;
 import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/sidebar';
 import axios from 'axios';
 import './dashboard.css';
-import {useNavigate} from 'react-router-dom'
-import dashboard from '../assets/Group.png'
+import { useNavigate } from 'react-router-dom';
+import dashboard from '../assets/Group.png';
 
 const Dashboard = () => {
     const [tableData, setTableData] = useState([]);
@@ -11,46 +228,28 @@ const Dashboard = () => {
     const [selectedUser, setSelectedUser] = useState('All');
     const [userOptions, setUserOptions] = useState([]);
     const [key, setKey] = useState(0);
+    const [searchQuery, setSearchQuery] = useState('');
 
     const navigate = useNavigate();
 
     useEffect(() => {
-
         const fetchTableData = async () => {
             try {
                 const response = await axios.get(`http://localhost:8000/rapidops/api/htmlFile/getHtmlbyfilter?status=${statusFilter}&user=${selectedUser}`);
-                console.log(selectedUser, statusFilter)
                 setTableData(response.data);
-                console.log(response.data)
             } catch (error) {
                 console.error('Error fetching table data:', error);
             }
         };
-        console.log(key)
 
         fetchTableData();
-    }, [statusFilter, selectedUser,key]);
-
-
-    
-
-    
+    }, [statusFilter, selectedUser, key]);
 
     useEffect(() => {
-        const html = localStorage.getItem('htmlId');
-        if(html){
-            localStorage.removeItem('htmlId')
-
-        }
-        // const uid = localStorage.getItem('uid');
-        // if(uid){
-            
-        // }
         const fetchUsers = async () => {
             try {
                 const response = await axios.get('http://localhost:8000/rapidops/api/users/getAllUsers');
                 setUserOptions(response.data);
-                
             } catch (error) {
                 console.error('Error fetching users:', error);
             }
@@ -67,7 +266,6 @@ const Dashboard = () => {
         setSelectedUser(event.target.value);
     };
 
-
     const getStatusColorClass = (status) => {
         switch (status) {
             case 'draft':
@@ -82,11 +280,8 @@ const Dashboard = () => {
     };
     
     const handleDeleteRow = async (rowId) => {
-        console.log(rowId)
         try {
-            const deleteHtml = await axios.delete(`http://localhost:8000/rapidops/api/htmlFile/deleteHtml/${rowId}`);
-            console.log(deleteHtml)
-
+            await axios.delete(`http://localhost:8000/rapidops/api/htmlFile/deleteHtml/${rowId}`);
             const updatedTableData = tableData.filter(row => row.id !== rowId);
             setTableData(updatedTableData);
             setKey(prevKey => prevKey + 1);
@@ -95,16 +290,24 @@ const Dashboard = () => {
         }
     };
 
-
-
     const handleEditRow = (rowId) => {
-        // Redirect to the edit page for the selected row or call edit API
-        localStorage.setItem('htmlId' , rowId)
-        
-        navigate('/homePage')
-        console.log('Editing row:', rowId);
-
+        localStorage.setItem('htmlId' , rowId);
+        navigate('/homePage');
     };
+
+    // Event handler to update search query state
+    const handleSearch = (event) => {
+        setSearchQuery(event.target.value);
+    };
+
+    // Filtering tableData based on search query
+    const filteredTableData = tableData.filter((row) =>
+        Object.values(row).some(
+            (value) =>
+                typeof value === 'string' &&
+                value.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+    );
 
     return (
         <div className='dashboardContainer'>
@@ -114,23 +317,19 @@ const Dashboard = () => {
                     <div className='leftSideItems'>
                         <div>logo</div>
                         <div className='left'>
-                        <span class="h4 font-weight-bold">Pages</span>
-
-                        <span>create and publish pages</span>
+                            <span className="h4 font-weight-bold">Pages</span>
+                            <span>create and publish pages</span>
                         </div>
                     </div>
                     <div className='rightSideItems'>
-                        <button type="button" className="publishButton" onClick={()=>{
-                                    navigate('/homePage')
-
-                        }}>+ Add Page</button>
+                        <button type="button" className="publishButton" onClick={() => navigate('/homePage')}>+ Add Page</button>
                     </div>
                 </div>
 
                 <div className='mainContent'>
                     <div className='searchRow'>
-                        <input type="text" placeholder="Search..." />
-                        <span>Entries: {tableData.length}</span>
+                        <input type="text" placeholder="Search..." className='search' onChange={handleSearch}/>
+                        <span>Entries: {filteredTableData.length}</span>
                         <select value={statusFilter} onChange={handleStatusChange}>
                             <option value="All">All</option>
                             <option value="draft">Draft</option>
@@ -147,8 +346,8 @@ const Dashboard = () => {
 
                     <div className='tableWrapper'>
                         <table className='dashboardTable'>
-                            <thead className='bg-white' >
-                                <tr >
+                            <thead className='bg-white'>
+                                <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Title</th>
                                     <th scope="col">URL</th>
@@ -160,20 +359,16 @@ const Dashboard = () => {
                                 </tr>
                             </thead>
                             <tbody>
-                                {tableData.map((row, index) => (
+                                {filteredTableData.map((row, index) => (
                                     <tr key={index}>
                                         <th scope="row">{index + 1}</th>
                                         <td className='tdtitle'>
-                                            <div>
-                                                {row.title}
-                                            </div>
-                                            <div class="dropdown" style={{display:'flex' , alignItems:'center' , justifyContent:'center'}}>
-                                                <button class="btn " type="button" id={`dropdownMenuButton-${index}`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                                    ...
-                                                </button>
-                                                <div class="dropdown-menu"  aria-labelledby={`dropdownMenuButton-${index}`}>
-                                                    <a class="dropdown-item" href="#" onClick={() => handleDeleteRow(row._id)} style={{color : 'red'}}>delete</a>
-                                                    <a class="dropdown-item" href="#" onClick={() => handleEditRow(row._id)}>edit</a>
+                                            <div>{row.title}</div>
+                                            <div className="dropdown" style={{ display:'flex', alignItems:'center', justifyContent:'center' }}>
+                                                <button className="btn" type="button" id={`dropdownMenuButton-${index}`} data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">...</button>
+                                                <div className="dropdown-menu" aria-labelledby={`dropdownMenuButton-${index}`}>
+                                                    <a className="dropdown-item" href="#" onClick={() => handleDeleteRow(row._id)} style={{ color: 'red' }}>delete</a>
+                                                    <a className="dropdown-item" href="#" onClick={() => handleEditRow(row._id)}>edit</a>
                                                 </div>
                                             </div>
                                         </td>
